3344ff6428b72a7a5d1c48bf8db3dd722008b5ad
mffernanda <ffmariaa77@gmail.com>
2026-09-16 08:34:29 -0300

    Códigos Atividade 1

---
+++ src/main/java/Atividade1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Atividade1 {
+
+    public class void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual o seu nome?");
+        String nome = leitor.nextLine();
+
+
+        leitor.close();
+    }
+}
